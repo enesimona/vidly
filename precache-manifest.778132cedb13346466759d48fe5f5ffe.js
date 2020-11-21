58bab5b1ece2874a013be43fbe4cdcbfd669440e
Simona Ene <enesimonam@gmail.com>
2020-11-21 12:31:01 +0200

    Updates

---
+++ precache-manifest.778132cedb13346466759d48fe5f5ffe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5549a06f28922ecca0c8f43761d65a08",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "2ce96812feda6b46964c",
+    "url": "/vidly/static/css/2.66afcd11.chunk.css"
+  },
+  {
+    "revision": "adce8fdf3170f4c8388f",
+    "url": "/vidly/static/css/main.97130cc6.chunk.css"
+  },
+  {
+    "revision": "2ce96812feda6b46964c",
+    "url": "/vidly/static/js/2.5addbc52.chunk.js"
+  },
+  {
+    "revision": "8ee726dd9774239537524d9592f95c64",
+    "url": "/vidly/static/js/2.5addbc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adce8fdf3170f4c8388f",
+    "url": "/vidly/static/js/main.19358d72.chunk.js"
+  },
+  {
+    "revision": "cb388922157c702ba0d5",
+    "url": "/vidly/static/js/runtime-main.f8720289.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
